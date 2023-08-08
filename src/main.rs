28221755afa7f4f0109a37c262cc5edd299da8dc
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-08-08 17:11:24 -0600

    test serialization

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,13 @@ mod chia_wallet;
 mod chiapos;
 mod program_utils;
 
-use crate::chia_wallet::cat::puzzles::create_cat_outer_puzzle::{
-    create_cat_outer_puzzlehash, create_cat_puzzle,
+use std::collections::HashMap;
+
+use crate::chia_wallet::{
+    cat::puzzles::create_cat_outer_puzzle::{create_cat_outer_puzzlehash, create_cat_puzzle},
+    core::serialization::{serialize_item, serialize_list, serialize_map, ToBytesMixin},
 };
 use api::hex_to_bytes;
-use chia_bls::{derive_keys::master_to_wallet_hardened, secret_key::SecretKey};
-use chia_wallet::standart::puzzles::calculate_synthetic_public_key;
-use clvmr::{
-    allocator::{Allocator, NodePtr},
-    node::Node,
-    serialize::{node_from_bytes, node_to_bytes},
-};
 
 use crate::{
     api::{
@@ -28,7 +24,7 @@ use crate::{
 
 fn main() {
     let acs = Program::from(1);
-   // let acs_ph = acs.clone().tree_hash().to_sized_bytes().to_vec();
+    // let acs_ph = acs.clone().tree_hash().to_sized_bytes().to_vec();
     let empyth_program_lst: Vec<Program> = vec![];
     let tail = Program::from(empyth_program_lst);
     let tail_hash = tail.tree_hash().to_sized_bytes().to_vec();
@@ -44,7 +40,7 @@ fn main() {
     let mut i = 0;
     loop {
         match args.next() {
-            Some(arg) => { 
+            Some(arg) => {
                 i = i + 1;
                 println!("arg: {} {}", i, (arg.clone()));
             }
@@ -54,6 +50,42 @@ fn main() {
 
     // println!("cat_puzzlehash1: {}", bytes_to_hex(cat_puzzle.serialized.clone()));
     println!("cat_puzzlehash1: {}", bytes_to_hex(cat_puzzlehash.clone()));
-    println!("tail_hash as atom: {}", bytes_to_hex(Program::from(cat_puzzlehash.clone()).serialized.clone()));
-    println!("tail_hash from serialized: {}", bytes_to_hex(Program::new(cat_puzzlehash.clone()).serialized.clone()));
+    println!(
+        "tail_hash as atom: {}",
+        bytes_to_hex(Program::from(cat_puzzlehash.clone()).serialized.clone())
+    );
+    println!(
+        "tail_hash from serialized: {}",
+        bytes_to_hex(Program::new(cat_puzzlehash.clone()).serialized.clone())
+    );
+
+    let number: i64 = 42;
+    let serialized_number = serialize_item(&number);
+    println!("Número serializado: {:?}", serialized_number);
+
+    // Ejemplo de uso para bool
+    let boolean: bool = true;
+    let serialized_boolean = serialize_item(&boolean);
+    println!("Booleano serializado: {:?}", serialized_boolean);
+
+    // Ejemplo de uso para String
+    let text: String = "Hello, Rust!".to_string();
+    let serialized_text = serialize_item(&text);
+    println!("Texto serializado: {:?}", serialized_text);
+
+    // Ejemplo de uso para HashMap
+    let mut map: HashMap<Box<dyn ToBytesMixin>, Box<dyn ToBytesMixin>> = HashMap::new();
+    map.insert(Box::new(42_i64), Box::new(true));
+    map.insert(Box::new("Key".to_string()), Box::new("Value".to_string()));
+    let serialized_map = serialize_map(&map);
+    println!("Mapa serializado: {:?}", serialized_map);
+
+    // Ejemplo de uso para Vec
+    let list: Vec<Box<dyn ToBytesMixin>> = vec![
+        Box::new("Apple".to_string()),
+        Box::new(false),
+        Box::new(123_i64),
+    ];
+    let serialized_list = serialize_list(&list);
+    println!("Lista serializada: {:?}", serialized_list);
 }

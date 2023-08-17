97c09c7e3ef4320b34fbb8735347e67d3be54e26
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-08-16 21:58:24 -0600

    make public attributes

--- src/chia_wallet/core/blockchain_network.rs
+++ src/chia_wallet/core/blockchain_network.rs
@@ -2,12 +2,12 @@ use serde::Deserialize; // Import serde's Deserialize trait
 
 #[derive(Debug, Deserialize)] // Derive the Deserialize trait for serialization
 pub struct BlockchainNetwork {
-    name: String,
-    unit: Option<String>,          // Use Option<String> for the nullable field
-    ticker: Option<String>,        // Use Option<String> for the nullable field
-    address_prefix: String,        // Use snake_case for field names
-    agg_sig_me_extra_data: String, // Use snake_case for field names
-    precision: Option<i32>,        // Use Option<i32> for the nullable field
-    fee: Option<i32>,              // Use Option<i32> for the nullable field
-    network_config: serde_json::Value, // Use serde_json::Value for dynamic data
+    pub name: String,
+    pub unit: Option<String>,   // Use Option<String> for the nullable field
+    pub ticker: Option<String>, // Use Option<String> for the nullable field
+    pub address_prefix: String, // Use snake_case for field names
+    pub agg_sig_me_extra_data: String, // Use snake_case for field names
+    pub precision: Option<i32>, // Use Option<i32> for the nullable field
+    pub fee: Option<i32>,       // Use Option<i32> for the nullable field
+    pub network_config: serde_json::Value, // Use serde_json::Value for dynamic data
 }

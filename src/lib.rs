c488a4aa4bc8838d3d26d6375756366d88504b6d
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-08-29 21:01:19 -0600

    generated with 1.79.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod api;
-mod program_utils;
 mod blockchain;
-mod chiapos;
+mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod chia_wallet;
- 
+mod chiapos;
+mod program_utils;
+
 #[no_mangle]
 pub extern "C" fn enforce_binding() {}

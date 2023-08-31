19bbe7331cef16d8803e045c7a1357fbb310930f
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-08-31 17:11:24 -0600

    debug build linux

--- build_linux_in_docker.sh
+++ build_linux_in_docker.sh
@@ -2,5 +2,9 @@
 
 # cargo zigbuild --target aarch64-unknown-linux-gnu --release 
 # cargo zigbuild --target x86_64-unknown-linux-gnu --release
-
+ 
 cargo build --release --target=x86_64-unknown-linux-gnu
+
+mkdir -p ./platform-build/web
+wasm-pack build --target web --release --out-dir ./platform-build/web
+tar -czvf web_rust_bls_flutter.tar.gz ./platform-build/web
--- build_with_dockers.sh
+++
@@ -1,2 +0,0 @@
-docker build ./../ -t rust_cross_compile/amd64 -f Dockerfile.amd64  
-docker run --rm -ti -v `pwd`:/chia_rust_utils   rust_cross_compile/amd64

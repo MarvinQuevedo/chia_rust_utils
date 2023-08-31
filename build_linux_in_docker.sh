5b3bd7337062c124990872bf738ccc67d5b541d4
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-08-31 17:36:19 -0600

    mv web rust to platform build

--- build_linux_in_docker.sh
+++ build_linux_in_docker.sh
@@ -8,3 +8,4 @@ cargo build --release --target=x86_64-unknown-linux-gnu
 mkdir -p ./platform-build/web
 wasm-pack build --target web --release --out-dir ./platform-build/web
 tar -czvf web_rust_bls_flutter.tar.gz ./platform-build/web
+mv web_rust_bls_flutter.tar.gz ./platform-build/ 
